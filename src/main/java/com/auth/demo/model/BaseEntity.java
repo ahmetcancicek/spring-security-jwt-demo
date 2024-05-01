2fa2ad2bf0a734013eef711d79cce762275396fd
Ahmet Can Cicek <ahmetcancicek@hotmail.com>
2024-05-01 21:22:55 +0300

    Refactor the base entity to have more clear code

--- src/main/java/com/auth/demo/model/BaseEntity.java
+++ src/main/java/com/auth/demo/model/BaseEntity.java
@@ -2,16 +2,12 @@ package com.auth.demo.model;
 
 import jakarta.persistence.*;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
 @MappedSuperclass
-public class BaseEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", updatable = false, nullable = false)
-    protected Long id;
+public class BaseEntity implements Serializable {
 
     @Column(name = "created_at", nullable = false, updatable = false)
     protected LocalDateTime createdAt;
@@ -33,13 +29,6 @@ public class BaseEntity {
     public BaseEntity() {
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     @Override
     public boolean equals(Object o) {
@@ -48,15 +37,13 @@ public class BaseEntity {
 
         BaseEntity that = (BaseEntity) o;
 
-        if (!Objects.equals(id, that.id)) return false;
         if (!Objects.equals(createdAt, that.createdAt)) return false;
         return Objects.equals(updatedAt, that.updatedAt);
     }
 
     @Override
     public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (createdAt != null ? createdAt.hashCode() : 0);
+        int result = createdAt != null ? createdAt.hashCode() : 0;
         result = 31 * result + (updatedAt != null ? updatedAt.hashCode() : 0);
         return result;
     }
