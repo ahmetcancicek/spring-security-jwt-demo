68d7ce5d6a325439b2c68f7117ca714de386eae0
Ahmet Can Cicek <ahmetcancicek@hotmail.com>
2024-06-25 18:22:09 +0300

    Implement the service for authentication

---
+++ src/main/java/com/auth/demo/service/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.auth.demo.service;
+
+import com.auth.demo.repository.UserRepository;
+import com.auth.demo.security.AuthenticatedUser;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByUsername(username);
+
+        return user.map(AuthenticatedUser::new)
+                .orElseThrow(() -> new UsernameNotFoundException(""));
+    }
+}
