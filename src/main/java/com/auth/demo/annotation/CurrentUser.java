f780a5258a0778a04b9b9b51fbd142dc00234d4c
Ahmet Can Cicek <ahmetcancicek@hotmail.com>
2024-06-26 10:11:57 +0300

    Add annotation for authentication principal

---
+++ src/main/java/com/auth/demo/annotation/CurrentUser.java
@@ -0,0 +1,12 @@
+package com.auth.demo.annotation;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.*;
+
+@Documented
+@AuthenticationPrincipal
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+public @interface CurrentUser {
+}
