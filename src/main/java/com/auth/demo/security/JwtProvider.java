1e8b8673581938b4494cc656923d209207a9e45c
Ahmet Can Cicek <ahmetcancicek@hotmail.com>
2024-06-27 11:28:10 +0300

    Refactor the process of JWT

--- src/main/java/com/auth/demo/security/JwtProvider.java
+++ src/main/java/com/auth/demo/security/JwtProvider.java
@@ -1,25 +1,31 @@
 package com.auth.demo.security;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class JwtProvider {
 
     private final JwtEncoder jwtEncoder;
+    private final JwtDecoder jwtDecoder;
 
-    public JwtProvider(JwtEncoder jwtEncoder) {
+    public JwtProvider(JwtEncoder jwtEncoder, JwtDecoder jwtDecoder) {
         this.jwtEncoder = jwtEncoder;
+        this.jwtDecoder = jwtDecoder;
     }
 
     public String generateToken(Authentication authentication) {
-        AuthenticatedUser principal = (AuthenticatedUser) authentication.getPrincipal();
+        AuthUser principal = (AuthUser) authentication.getPrincipal();
         var now = Instant.now();
 
         // TODO: Check the claim properties
@@ -27,10 +33,23 @@ public class JwtProvider {
                 .issuer("self")
                 .issuedAt(now)
                 .expiresAt(now.plus(10, ChronoUnit.HOURS))
-                .subject(authentication.getName())
-                .claim("scope", "ROLE_USER")
+                .subject(((AuthUser) authentication.getPrincipal()).getUser().getUsername())
+                .claim("authorities", "ROLE_USER")
                 .build();
 
         return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+    public String getUsernameFromJWT(String token) {
+        Jwt jwt = jwtDecoder.decode(token);
+        return jwtDecoder.decode(token).getSubject();
+    }
+
+    public List<GrantedAuthority> getAuthoritiesFromJWT(String token) {
+        Jwt jwt = jwtDecoder.decode(token);
+        Map<String, Object> claims = jwt.getClaims();
+        return Arrays.stream(claims.get("authorities").toString().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
     }
 }
---
+++ src/main/java/com/auth/demo/security/JwtValidator.java
@@ -0,0 +1,7 @@
+package com.auth.demo.security;
+
+public class JwtValidator {
+    public boolean validateToken(String jwt) {
+        return true;
+    }
+}
