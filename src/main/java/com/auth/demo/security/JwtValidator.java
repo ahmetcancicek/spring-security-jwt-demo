8a51e5216bfc856ee94e6e7b1ab4034653cca35b
Ahmet Can Cicek <ahmetcancicek@hotmail.com>
2024-06-28 11:46:38 +0300

    Add TODO

--- src/main/java/com/auth/demo/security/JwtValidator.java
+++ src/main/java/com/auth/demo/security/JwtValidator.java
@@ -2,6 +2,7 @@ package com.auth.demo.security;
 
 public class JwtValidator {
     public boolean validateToken(String jwt) {
+        // TODO: Implement the token validation control
         return true;
     }
 }
