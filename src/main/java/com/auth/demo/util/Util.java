eb9218324eff5a038f18b2a656352af016af7405
Ahmet Can Cicek <ahmetcancicek@hotmail.com>
2024-07-01 22:32:46 +0300

    Add Util class

---
+++ src/main/java/com/auth/demo/util/Util.java
@@ -0,0 +1,10 @@
+package com.auth.demo.util;
+
+import java.util.UUID;
+
+public class Util {
+
+    public static String generateRandomUUID() {
+        return UUID.randomUUID().toString();
+    }
+}
