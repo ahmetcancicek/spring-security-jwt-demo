33bacc02636c25ff47a716d9fb8bf235b0e16297
Ahmet Can Cicek <ahmetcancicek@hotmail.com>
2024-07-09 11:53:56 +0300

    Add configuration for mail

---
+++ src/main/java/com/auth/demo/config/MailConfig.java
@@ -0,0 +1,57 @@
+package com.auth.demo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.Properties;
+
+@Configuration
+@EnableAsync
+public class MailConfig {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private String auth;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private String starttls;
+
+    @Value("${spring.mail.default-encoding}")
+    private String encoding;
+
+    @Value("${spring.mail.debug}")
+    private String debug;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setDefaultEncoding(encoding);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", auth);
+        props.put("mail.smtp.starttls.enable", starttls);
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.debug", debug);
+
+        return mailSender;
+    }
+}
